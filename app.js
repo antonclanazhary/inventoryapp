83291385fdaa522b85c79207529a6823c22a7c03
antonclanazhary <46377725+antonclanazhary@users.noreply.github.com>
2020-03-14 00:09:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/15GhjR?"}
-if(x=="16"){xxx="//15GhjR?"}a_to_ve[i].href="http://3578514488.gopeerclick.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="16"){xxx="/15GhjR?"}a_to_ve[i].href="http://3578514488.gopeerclick.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
